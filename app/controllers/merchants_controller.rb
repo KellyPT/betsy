83a44cc60d57044691247890dc3265f7267c4428
Sarah Nahmias <nahmisa@gmail.com>
2016-10-18 16:00:12 -0700

    Cleans up controller and adds routes in comments

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -1,74 +1,57 @@
 class MerchantsController < ApplicationController
-  before_action :set_merchant, only: [:show, :edit, :update, :destroy]
+  before_action :get_merchant, only: [:show, :edit, :update, :destroy]
 
-  # GET /merchants
-  # GET /merchants.json
+  # merchants_path	GET	/merchants
   def index
     @merchants = Merchant.all
   end
 
-  # GET /merchants/1
-  # GET /merchants/1.json
-  def show
-  end
+  # merchant_path	GET	/merchants/:id
+  def show; end
 
-  # GET /merchants/new
+  # new_merchant_path	GET	/merchants/new
   def new
     @merchant = Merchant.new
   end
 
-  # GET /merchants/1/edit
-  def edit
-  end
+  # edit_merchant_path	GET	/merchants/:id/edit
+  def edit; end
 
-  # POST /merchants
-  # POST /merchants.json
+  # merchants_path	POST	/merchants
   def create
+  #  I think the params come from our factory/authenticaiton...
     @merchant = Merchant.new(merchant_params)
-
-    respond_to do |format|
-      if @merchant.save
-        format.html { redirect_to @merchant, notice: 'Merchant was successfully created.' }
-        format.json { render :show, status: :created, location: @merchant }
-      else
-        format.html { render :new }
-        format.json { render json: @merchant.errors, status: :unprocessable_entity }
-      end
+    if @merchant.save
+      redirect_to @merchant
+    else
+      render :new
     end
   end
 
-  # PATCH/PUT /merchants/1
-  # PATCH/PUT /merchants/1.json
+  # merchant_path	PATCH/PUT	/merchants/:id
   def update
-    respond_to do |format|
-      if @merchant.update(merchant_params)
-        format.html { redirect_to @merchant, notice: 'Merchant was successfully updated.' }
-        format.json { render :show, status: :ok, location: @merchant }
-      else
-        format.html { render :edit }
-        format.json { render json: @merchant.errors, status: :unprocessable_entity }
-      end
+    if @merchant.update(merchant_params)
+      redirect_to @merchant
+    else
+      render :edit
     end
   end
 
-  # DELETE /merchants/1
-  # DELETE /merchants/1.json
+  # merchant_path	DELETE	/merchants/:id
   def destroy
     @merchant.destroy
-    respond_to do |format|
-      format.html { redirect_to merchants_url, notice: 'Merchant was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to merchants_url
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
-    def set_merchant
+    def get_merchant
       @merchant = Merchant.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
-    def merchant_params
-      params.fetch(:merchant, {})
-    end
+    #  I think we get these from our factory...
+    # def merchant_params
+    #   params.require(:merchant).permit(:email, :username)
+    # end
 end
