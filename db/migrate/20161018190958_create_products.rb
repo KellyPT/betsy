eb378c3988c0c73400eedc4a0ff8e5505aab5641
Beylul <bkbreab8@gmail.com>
2016-10-18 14:23:47 -0700

    Added model migration relationships and tables

--- db/migrate/20161018190958_create_products.rb
+++ db/migrate/20161018190958_create_products.rb
@@ -1,6 +1,11 @@
 class CreateProducts < ActiveRecord::Migration[5.0]
   def change
     create_table :products do |t|
+      t.belongs_to :merchant, index: true
+      t.belongs_to :category, index: true
+      t.string :name
+      t.float :price
+      t.integer :quantity
 
       t.timestamps
     end
--- db/migrate/20161018191639_create_order_items.rb
+++ db/migrate/20161018191639_create_order_items.rb
@@ -1,8 +1,11 @@
 class CreateOrderItems < ActiveRecord::Migration[5.0]
   def change
     create_table :order_items do |t|
-
-      t.timestamps
+     t.belongs_to :order, index: true
+     t.belongs_to :product, index: true
+     t.integer :quantity
+     
+     t.timestamps
     end
   end
 end
