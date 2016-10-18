73deacc663d5ddf19bed1ee5891afd727ef17a72
Sarah Nahmias <nahmisa@gmail.com>
2016-10-18 15:41:22 -0700

    Cleans up controller and adds routes

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -1,74 +1,56 @@
 class OrderItemsController < ApplicationController
-  before_action :set_order_item, only: [:show, :edit, :update, :destroy]
+  before_action :get_order_item, only: [:show, :edit, :update, :destroy]
 
-  # GET /order_items
-  # GET /order_items.json
+  # order_order_items_path	GET	/orders/:order_id/order_items
   def index
     @order_items = OrderItem.all
   end
 
-  # GET /order_items/1
-  # GET /order_items/1.json
-  def show
-  end
+  # order_order_item_path	GET	/orders/:order_id/order_items/:id
+  def show; end
 
-  # GET /order_items/new
+  # new_order_order_item_path	GET	/orders/:order_id/order_items/new
   def new
     @order_item = OrderItem.new
   end
 
-  # GET /order_items/1/edit
-  def edit
-  end
+  # edit_order_order_item_path	GET	/orders/:order_id/order_items/:id/edit
+  def edit; end
 
-  # POST /order_items
-  # POST /order_items.json
+  # order_order_items_path POST	/orders/:order_id/order_items
   def create
     @order_item = OrderItem.new(order_item_params)
-
-    respond_to do |format|
-      if @order_item.save
-        format.html { redirect_to @order_item, notice: 'Order item was successfully created.' }
-        format.json { render :show, status: :created, location: @order_item }
-      else
-        format.html { render :new }
-        format.json { render json: @order_item.errors, status: :unprocessable_entity }
-      end
+    if @order_item.save
+     redirect_to @order_item
+    else
+      render :new
     end
   end
 
-  # PATCH/PUT /order_items/1
-  # PATCH/PUT /order_items/1.json
+  # order_order_items_path PATCH/PUT	/orders/:order_id/order_items/:id
   def update
-    respond_to do |format|
-      if @order_item.update(order_item_params)
-        format.html { redirect_to @order_item, notice: 'Order item was successfully updated.' }
-        format.json { render :show, status: :ok, location: @order_item }
-      else
-        format.html { render :edit }
-        format.json { render json: @order_item.errors, status: :unprocessable_entity }
-      end
+    if @order_item.update(order_item_params)
+      redirect_to @order_item
+    else
+      render :edit
     end
   end
 
-  # DELETE /order_items/1
-  # DELETE /order_items/1.json
+  # order_order_items_path DELETE	/orders/:order_id/order_items/:id
   def destroy
     @order_item.destroy
-    respond_to do |format|
-      format.html { redirect_to order_items_url, notice: 'Order item was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to order_items_url
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
-    def set_order_item
+    def get_order_item
       @order_item = OrderItem.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
-    def order_item_params
-      params.fetch(:order_item, {})
-    end
+    # I don't think we actually need a param for this because we shouldn't set it through the view???
+    # def order_item_params
+    #   params.require(:order_item).permit(:quantity)
+    # end
 end
