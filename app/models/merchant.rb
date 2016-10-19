a8e7e255a8b436e59db19ce3d1b8912c707fb276
Sarah Nahmias <nahmisa@gmail.com>
2016-10-19 10:07:59 -0700

    Adds validation to merchant

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,4 +1,16 @@
 class Merchant < ApplicationRecord
   has_many :products
+  validates :email, :user_name, presence: true
+  
+  #  :uid, :provider,
+  # def self.build_from_github(auth_hash)
+  #   user          = User.new
+  #   user.uid      = auth_hash[:uid]
+  #   user.provider = 'github'
+  #   user.name     = auth_hash['info']['name']
+  #   user.email    = auth_hash['info']['email']
+  #
+  #   return user
+  # end
 
 end
