9957658ecd7f28cb4eb4677545c7ca2a97c3e94e
Sarah Nahmias <nahmisa@gmail.com>
2016-10-18 21:00:17 -0700

    Removes scaffolding excess from categories controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,74 +1,60 @@
 class CategoriesController < ApplicationController
-  before_action :set_category, only: [:show, :edit, :update, :destroy]
+  before_action :get_category, only: [:show, :edit, :update, :destroy]
 
-  # GET /categories
-  # GET /categories.json
+  # categories GET    /categories
   def index
     @categories = Category.all
   end
 
-  # GET /categories/1
-  # GET /categories/1.json
-  def show
-  end
+  # new_category GET    /categories/new
+  def show; end
 
   # GET /categories/new
   def new
     @category = Category.new
   end
 
-  # GET /categories/1/edit
-  def edit
-  end
+  # def edit; end
 
-  # POST /categories
-  # POST /categories.json
+  # categories POST   /categories
   def create
     @category = Category.new(category_params)
 
-    respond_to do |format|
-      if @category.save
-        format.html { redirect_to @category, notice: 'Category was successfully created.' }
-        format.json { render :show, status: :created, location: @category }
-      else
-        format.html { render :new }
-        format.json { render json: @category.errors, status: :unprocessable_entity }
-      end
+    if @category.save
+      redirect_to @category
+    else
+      render :new
     end
   end
 
-  # PATCH/PUT /categories/1
-  # PATCH/PUT /categories/1.json
-  def update
-    respond_to do |format|
-      if @category.update(category_params)
-        format.html { redirect_to @category, notice: 'Category was successfully updated.' }
-        format.json { render :show, status: :ok, location: @category }
-      else
-        format.html { render :edit }
-        format.json { render json: @category.errors, status: :unprocessable_entity }
-      end
-    end
-  end
 
-  # DELETE /categories/1
-  # DELETE /categories/1.json
+  # def update
+  #   respond_to do |format|
+  #     if @category.update(category_params)
+  #       format.html { redirect_to @category, notice: 'Category was successfully updated.' }
+  #       format.json { render :show, status: :ok, location: @category }
+  #     else
+  #       format.html { render :edit }
+  #       format.json { render json: @category.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
+
+  # catergory DELETE /categories/:id
   def destroy
     @category.destroy
-    respond_to do |format|
-      format.html { redirect_to categories_url, notice: 'Category was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to categories_url
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
-    def set_category
+    def get_category
       @category = Category.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
-    def category_params
-      params.fetch(:category, {})
-    end
+    # I don't think we should be able to edit catergories, only add so I don't think we need this
+    # def category_params
+    #   params.require(:category).permit(:name)
+    # end
 end
