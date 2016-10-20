86c77c7ec9c74fe1eb36777345d0c908f030b0b6
Sarah Nahmias <nahmisa@gmail.com>
2016-10-20 14:47:51 -0700

    Adds the quantity to order item

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -6,6 +6,7 @@ class OrderItem < ApplicationRecord
     order_item = self.new
     order_item.order_id = this_order_id
     order_item.product_id = this_product_id
+    order_item.quantity = 1
     return order_item
   end
 
