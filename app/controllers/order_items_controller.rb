b6e3daa94f5a45b334d0762cb1c83ccc73e4d40e
Sarah Nahmias <nahmisa@gmail.com>
2016-10-20 21:05:39 -0700

    Refactors redirect in orders#create out

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -7,22 +7,9 @@ class OrderItemsController < ApplicationController
     @order_items = @order.order_items
   end
 
-  def new
-  # order controller is redirected here from create (order)
-  # when adding an item to the order.  Redirect through a GET
-    @order_item = OrderItem.build_order_item(session[:order_id], session[:product_id] )
-
-    if @order_item.save
-      redirect_to order_order_items_path
-    else
-      flash[:error] = "Could not add product to cart"
-      redirect_to root
-    end
-  end
-
   def update
     product = @order_item.product
-    if product.check_availability(order_item_params["quantity"])
+    if product.update_quantity(order_item_params["quantity"])
       @order_item.update(order_item_params)
     else
       flash[:error] = "Could not increase item quantity"
--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -2,7 +2,7 @@ class OrderItem < ApplicationRecord
   belongs_to :product
   belongs_to :order
 
-  def self.build_order_item(this_order_id, this_product_id)
+  def self.add_order_item_to_order(this_order_id, this_product_id)
     order_item = self.new
     order_item.order_id = this_order_id
     order_item.product_id = this_product_id
