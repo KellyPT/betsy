135eb606e907b6cb45a37c016d675d5da478158f
Sarah Nahmias <nahmisa@gmail.com>
2016-10-21 14:09:03 -0700

    Corrects skip before action from only to except in products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,7 +2,7 @@ class ProductsController < ApplicationController
   before_action :get_product, only: [:show, :edit, :update, :destroy]
 
   # Kelly: I don't know how to use controller filter here yet. So I will temporarily skip authentication requirements.
-  skip_before_action :require_login, only [:new, :create]
+  skip_before_action :require_login, except: [:new, :create]
 
   # products_path	GET	/products
   # merchant_products_path	GET	/merchants/:merchant_id/products
