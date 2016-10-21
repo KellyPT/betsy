62ceef725bd1328648e7e86bcca74442cc2da99a
Sarah Nahmias <nahmisa@gmail.com>
2016-10-20 20:13:26 -0700

    Cleans up order item model

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -9,24 +9,4 @@ class OrderItem < ApplicationRecord
     order_item.quantity = 1
     return order_item
   end
-
-  def add_item(order_item)
-    if order_item.quantity
-      order_item.quantity += 1
-    else
-      false
-    end
-  end
-
-  # subtract an item from the quantity if able
-  # else returns false
-  def remove_item(order_item)
-    if order_item.quantity > 0
-      order_item.quantity -= 1
-    else
-      false
-    end
-  end
-
-# display order item method that shows only if order items are > 0?
 end
