d78f85a6238e06c17852103917cffb67413a116b
guineveresaenger <guineveresaenger@yahoo.com>
2016-10-23 16:48:33 -0700

    Test and validations for OrderItem

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -2,11 +2,13 @@ class OrderItem < ApplicationRecord
   belongs_to :product
   belongs_to :order
 
+  validates :quantity, numericality: {greater_than: 0}
+
   def self.add_order_item_to_order(this_order_id, this_product_id)
-    order_item = self.new
+    order_item = self.new(quantity: 1)
     order_item.order_id = this_order_id
     order_item.product_id = this_product_id
-    order_item.quantity = 1
+    # order_item.quantity = 1
     return order_item
   end
 end
--- test/models/order_item_test.rb
+++ test/models/order_item_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class OrderItemTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "Create an OrderItem with valid data" do
+    order_item = order_items(:one)
+    assert order_item.valid?
+  end
+
+  test "Must have positive quantity" do
+    order_item1 = order_items(:one)
+    order_item2 = OrderItem.new(quantity: -4)
+    assert order_item1.valid?
+    assert_not order_item2.valid?
+
+    assert_includes order_item2.errors, :quantity
+
+  end
+
+  # TODO: cannot test Class method in Model tests, or can I? not sure
+
 end
