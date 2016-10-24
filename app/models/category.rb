9758408700b6ec5a5712434bb8962aed328fdfd0
guineveresaenger <guineveresaenger@yahoo.com>
2016-10-23 19:38:10 -0700

    Added validations, tests and fixtures for Category

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,6 @@
 class Category < ApplicationRecord
   has_many :products, through: :categories_products
   has_many :categories_products
+
+  validates :name, presence: true, uniqueness: true
 end
--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -1,7 +1,24 @@
 require 'test_helper'
 
 class CategoryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "Create a Category with valid data" do
+    category = categories(:one)
+    assert category.valid?
+  end
+
+  test "Category must have a name" do
+    category = Category.new
+    assert_not category.valid?
+    assert_includes category.errors, :name
+  end
+
+  test "Category name must be unique" do
+    category1 = categories(:two)
+    category2 = Category.new(name: "Klingon Warbirds")
+    assert category1.valid?
+    assert_not category2.valid?
+    assert_includes category2.errors, :name
+  end
+
 end
