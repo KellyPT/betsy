214779130cbe943d763fa185d2c0318d55c7c9a2
Sarah Nahmias <nahmisa@gmail.com>
2016-10-25 12:38:54 -0700

    Adds ability to get merchant order items by all and status

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,7 +1,7 @@
 class Merchant < ApplicationRecord
   has_many :products
   validates :email, :uid, :provider, presence: true
-  
+
   def self.build_from_github(auth_hash)
     merchant           = Merchant.new
     merchant.uid       = auth_hash[:uid]
@@ -12,17 +12,31 @@ class Merchant < ApplicationRecord
     return merchant
   end
 
-  # validates :email, :user_name, presence: true
+  def get_merchant_orders
+    merchant_orders = []
+    products = self.products
+    products.each do |product|
+      order_items = product.order_items
+        order_items.each do |item|
+          merchant_orders << item
+        end
+    end
+    return merchant_orders
+  end
 
-  #  :uid, :provider,
-  # def self.build_from_github(auth_hash)
-  #   user          = User.new
-  #   user.uid      = auth_hash[:uid]
-  #   user.provider = 'github'
-  #   user.name     = auth_hash['info']['name']
-  #   user.email    = auth_hash['info']['email']
-  #
-  #   return user
-  # end
+  def get_merchant_orders_by_status(status)
+    merchant_orders = []
+    products = self.products
+    products.each do |product|
+      order_items = product.order_items
+      order_items.each do |item|
+        order = item.order
+        if order.order_status == status
+          merchant_orders << item
+        end
+      end
+    end
+    return merchant_orders
+  end
 
 end
