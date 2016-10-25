f482b49e801a737e299a059823489dd49ab42dbf
Sarah Nahmias <nahmisa@gmail.com>
2016-10-25 11:44:00 -0700

    Adds test for sum method

--- test/models/order_item_test.rb
+++ test/models/order_item_test.rb
@@ -16,9 +16,23 @@ class OrderItemTest < ActiveSupport::TestCase
 
   end
 
-  test "Will create a total sum for all the order items associated with an order upon purchasing" do
+  test "Will create a total sum for all the order items associated with an order group" do
     # @order_items.sum_total_prices
+      order = orders(:purchasing_order)
+      order_items_group = order.order_items
 
+      method_sum = OrderItem.sum_total_prices(order_items_group)
+
+      product1 = products(:product_stock1)
+      product2 = products(:product_stock2)
+
+      quantity1 = order_items(:reduce_quantity1).quantity
+      quantity2 = order_items(:reduce_quantity2).quantity
+
+      sub_total1 = product1.price * quantity1
+      sub_total2 = product2.price * quantity2
+
+      assert_equal(method_sum, sub_total1 + sub_total2)
   end
 
 end
