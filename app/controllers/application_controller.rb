a747894a7152e09a61860dd9a70a33cc48e9fc96
Beylul <bkbreab8@gmail.com>
2016-10-27 08:30:24 -0700

    added welcome user_name function. Added functionality to view merchants and produts from the front page.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :require_login
+  helper_method :category, :merchant, :current_merchant
 
   def current_merchant
     @current_merchant ||= Merchant.find(session[:merchant_id]) if session[:merchant_id]
@@ -12,5 +13,11 @@ class ApplicationController < ActionController::Base
       flash[:error] = "You have been logged out. Please continue to browse our website as a Guest User."
       redirect_to root_path
     end
+  end
+  def category
+    Category.all
+  end
+  def merchant
+    Merchant.all
   end
 end
