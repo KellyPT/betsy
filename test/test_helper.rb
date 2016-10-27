a03618b4cd3da526243162c68ec02ba92b5c5980
Kelly Tran <trankphuong@gmail.com>
2016-10-27 14:20:55 -0700

    remove view_helpers from simplecov

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,6 +5,7 @@ require 'simplecov'
 SimpleCov.start do
   add_filter "/test/"
   add_filter "/config/"
+  add_filter "/helpers"
 end
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
