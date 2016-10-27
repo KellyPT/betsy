ce131055f5406f05645010782f5a8e45409dfcb5
Kelly Tran <trankphuong@gmail.com>
2016-10-27 09:56:31 -0700

    Created setup for OmniAuth in test_helper. Wrote tests for Sessions Controller : authentication.

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -1,7 +1,37 @@
 require 'test_helper'
 
 class SessionsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def login_a_user
+    request.env['omniauth.auth'] = OmniAuth.config.mock_auth[:github]
+    get :create, { provider: "github" }
+  end
+
+  test "can create a Merchant user" do
+    assert_difference('Merchant.count', 1) do
+      login_a_user
+      assert_response :redirect
+      assert_redirected_to sessions_log_in_path
+      assert_not_nil session[:merchant_id]
+    end
+  end
+
+  test "logging in twice should not create 2 Merchant users" do
+    assert_difference('Merchant.count', 1) do
+      login_a_user
+    end
+
+    assert_no_difference('Merchant.count') do
+      login_a_user
+      assert_response :redirect
+      assert_redirected_to sessions_log_in_path
+      assert_not_nil session[:merchant_id]
+    end
+  end
+
+  test "a not-logged-in Merchant user cannot see Merchant Control page" do
+    session[:merchant_id] = nil
+    get :index_log_out
+    assert_template 'sessions/index'
+    assert_response :success
+  end
 end
